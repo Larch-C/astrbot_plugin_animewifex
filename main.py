b7240f29752e6949760b3b9d6c8ba1aeb0771f40
monbed <67903399+monbed@users.noreply.github.com>
2025-04-27 18:45:35 +0800

    路径统一小写

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import re
 import json
 import aiohttp
 
-PLUGIN_DIR = os.path.join('data', 'AnimeWifeXdata')
+PLUGIN_DIR = os.path.join('data', 'animewifexdata')
 os.makedirs(PLUGIN_DIR, exist_ok=True)
 
 CONFIG_DIR = os.path.join(PLUGIN_DIR, 'config')
